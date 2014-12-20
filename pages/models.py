de5dcba663031d4d8d5600fed7e51c975bd0a6f1
Kacper Kołodziej <kacper@kolodziej.in>
2014-12-20 16:31:07 +0100

    added model of Pages

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from pywws.settings import LANGUAGES
 
-# Create your models here.
+class Page(models.Model):
+	title = models.CharField(max_length=55)
+	slug = models.SlugField(max_length=55)
+	content = models.TextField()
+	mod_date = models.DateField()
+	lang = models.CharField(max_length=5, choices=LANGUAGES)
+	published = models.BooleanField(default=False)
+	deleted = models.BooleanField(default=False)
+	related_pages = models.ManyToManyField("self", blank=True)
+
+	def __str__(self):
+		return self.title
